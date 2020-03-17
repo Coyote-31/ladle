a5a0c387e3680cb7835ed770e2e321cf76298549
Coyote-31 <coyotepouil@gmail.com>
2020-03-17 17:44:49 +0100

    🚧 Refactor every POM & dependencies

--- ladle-webapp/src/main/java/org/ladle/webapp/filter/EncodingFilter.java
+++ ladle-webapp/src/main/java/org/ladle/webapp/filter/EncodingFilter.java
@@ -1,6 +1,7 @@
 package org.ladle.webapp.filter;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -12,37 +13,41 @@ import javax.servlet.annotation.WebFilter;
 /**
  * Servlet Filter implementation class EncodingFilter
  */
-@WebFilter(filterName="encodingFilter", urlPatterns="/*")
+@WebFilter(filterName = "encodingFilter", urlPatterns = "/*")
 public class EncodingFilter implements Filter {
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// If you have any <init-param> in web.xml, then you could get them
-		// here by config.getInitParameter("name") and assign it as field.
-		//TODO
-	}
+  /**
+   * @see Filter#init(FilterConfig)
+   */
+  @Override
+  public void init(FilterConfig fConfig) throws ServletException {
+    // If you have any <init-param> in web.xml, then you could get them
+    // here by config.getInitParameter("name") and assign it as field.
+    // TODO
+  }
 
+  /**
+   * Encode en UTF8 les POST des Servlets
+   *
+   * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+   */
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
 
-	/**
-	 * Encode en UTF8 les POST des Servlets
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    request.setCharacterEncoding("UTF-8");
 
-		request.setCharacterEncoding("UTF-8");
+    // pass the request along the filter chain
+    chain.doFilter(request, response);
+  }
 
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
-	}
-
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// If you have assigned any expensive resources as field of
-		// this Filter class, then you could clean/close them here.
-	}
+  /**
+   * @see Filter#destroy()
+   */
+  @Override
+  public void destroy() {
+    // If you have assigned any expensive resources as field of
+    // this Filter class, then you could clean/close them here.
+  }
 
 }
