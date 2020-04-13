615ae54973421efab9cc4211e4d2c52e894bf8c8
Coyote-31 <coyotepouil@gmail.com>
2020-04-13 13:37:11 +0200

    ✨ Add basic stayConnected

--- ladle-webapp/src/main/java/org/ladle/webapp/servlet/Connexion.java
+++ ladle-webapp/src/main/java/org/ladle/webapp/servlet/Connexion.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -55,6 +56,9 @@ public class Connexion extends HttpServlet {
     String pwd = request.getParameter("login_InputPassword");
     LOG.debug("getParam pwd : {}", pwd);
 
+    String stayConnected = request.getParameter("login_CheckStayConnected");
+    LOG.debug("getParam stayConnected : {}", stayConnected);
+
     // Test de validation du formulaire
     boolean isLoginValid = userHandler.isLoginValid(login, pwd);
     LOG.debug("isLoginValid : {}", isLoginValid);
@@ -63,13 +67,53 @@ public class Connexion extends HttpServlet {
     HttpSession session = request.getSession();
     session.setAttribute("isLoginValid", isLoginValid);
 
-    // TODO renvoyer user.login pour la reconnexion
+    // Variable de cookie de connexion
+    boolean cookieLoginExist = false;
 
     // Si la connexion est validée
     if (isLoginValid) {
 
+      // Ajoute la variable des données utilisateur
       Utilisateur utilisateur = userHandler.getUtilisateurOnLogin(login);
       session.setAttribute("utilisateur", utilisateur);
+
+      // Si "rester connecté" est coché
+      if ("true".equals(stayConnected)) {
+        // gestion des cookies de connexion
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+          for (Cookie cookie : cookies) {
+            if ("login".equals(cookie.getName())) {
+              cookie.setValue(login);
+              cookie.setMaxAge(60 * 60 * 24 * 30);
+              LOG.debug("Update cookieLogin : {}", cookie.getValue());
+              cookieLoginExist = true;
+            }
+          }
+        }
+
+        if (!cookieLoginExist) {
+          Cookie cookieLogin = new Cookie("login", login);
+          cookieLogin.setMaxAge(60 * 60 * 24 * 30);
+          response.addCookie(cookieLogin);
+          LOG.debug("Add cookieLogin : {}", cookieLogin.getValue());
+        }
+
+      } else {
+        // suppression des cookies de connexion
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+          for (Cookie cookie : cookies) {
+            if ("login".equals(cookie.getName())) {
+              cookie.setValue(null);
+              cookie.setMaxAge(0);
+              response.addCookie(cookie);
+              LOG.debug("Delete cookieLogin");
+            }
+          }
+        }
+      }
+
     }
 
     getServletContext().getRequestDispatcher("/WEB-INF/connexion.jsp").forward(request, response);
--- ladle-webapp/src/main/webapp/WEB-INF/parts/header.jsp
+++ ladle-webapp/src/main/webapp/WEB-INF/parts/header.jsp
@@ -34,7 +34,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="login_ModalLongTitle">Modal title</h5>
+        <h5 class="modal-title" id="login_ModalLongTitle">Connexion</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
           aria-hidden="true">&times;</span></button>
       </div>
@@ -68,8 +68,8 @@
             <%-- Input checkBox --%>
             <div class="col">
               <div class="form-group form-check">
-                <input type="checkbox" name="login_CheckStayConnected" id="login_CheckStayConnected"
-                 class="form-check-input">
+                <input type="checkbox" checked name="login_CheckStayConnected" id="login_CheckStayConnected"
+                 class="form-check-input" value="true">
                 <label class="form-check-label" for="login_CheckStayConnected"
                 >Rester connecté</label>
               </div>
@@ -87,7 +87,6 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
